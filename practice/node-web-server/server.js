ab592718f9e6f6c84a98af313ca1b8a474a7b147
Gopinath Ponnusamy <gopinath.ponnusamy@aspiresys.com>
2018-06-27 18:40:02 +0530

    express with router,templating & middleware

---
+++ practice/node-web-server/server.js
@@ -0,0 +1,52 @@
+const express = require("express");
+const hbs = require("hbs");
+const fs = require("fs");
+
+var app = express();
+hbs.registerPartials(__dirname + "/views/partials");
+
+/*app.use((req,res,next) => {
+    res.render("maintanace.hbs", {});
+});
+*/
+app.use(express.static(__dirname + "/public"));
+
+app.use((req,res,next) => {
+    var now = new Date();
+    log = `${now}: ${req.method} ${req.path}\n`;
+    fs.appendFile("server.log", log, (error)=> {
+        if(error) {
+            console.log("unable to append to file");
+        }
+    });
+    next();
+});
+
+
+hbs.registerHelper("getCurrentYear", () => {
+    return new Date().getFullYear();
+});
+hbs.registerHelper("screamIt", (text) => {
+    return text.toUpperCase();
+});
+/*app.get("/", (req, res) => {
+    res.send({name:"gopinat"});
+});*/
+app.get("/", (req, res) => {
+    res.render('home.hbs', {
+        pageTitle: "Home",
+        welcomeMessage: "Welcome to sunday samaiyal"
+    });
+});
+
+app.get("/bad", (req, res) => {
+    res.send({errorMessage: "Unable to process request"});
+});
+
+app.get("/about", (req, res) => {
+    res.render('about.hbs', {
+        pageTitle: "About Us"
+    })
+});
+
+app.listen(3000);
